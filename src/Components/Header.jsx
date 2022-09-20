5d68b2c342e03140df6ae6c9225c50cd7c302d0e
MarkDoroshenko <msv0804@gmail.com>
2022-09-20 17:21:02 +0300

    id

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <img className='logoImg' src='https://cdn.logojoy.com/wp-content/uploads/2018/05/30143708/1113.png'
+                 alt='logo'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+                <img className='contentImg'
+                     src='https://kartinkin.net/uploads/posts/2021-07/thumbs/1625670745_8-kartinkin-com-p-oboi-na-dva-monitora-krasivie-9.png'
+                     alt='img'/>
+            </div>
+            <div>
+                <img className='profileImg'
+                     src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYUfbUNJrtXWHm5PrsKkvgbSm2XaRgX4tVhg&usqp=CAU'
+                     alt='img'/>
+                discription+++
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
